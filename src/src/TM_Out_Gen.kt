6ebf1ca02feacaa76e0092e4e12417e14c92ae0c
MayneKeen <menpet2000@gmail.com>
2019-06-26 06:29:11 +0300

    Initial

---
+++ src/src/TM_Out_Gen.kt
@@ -0,0 +1,8 @@
+package src
+                                                                            /*should contain a copy of current tape
+                                                                            *and some methods to make an output
+                                                                            */
+
+class TM_Out_Gen {
+
+}
---
+++ src/src/TM_UI_Parser.kt
@@ -0,0 +1,19 @@
+package src
+class TM_UI_Parser {                                             /*have been seen parsing user input including
+                                                                  *"in" files and console input
+                                                                  *
+                                                                  */
+
+    //sample
+
+
+    fun parseStates(inputFile:String):List<String> {
+
+        TODO()
+    }
+
+    fun parseTape(inputFile:String):List<String> {
+
+        TODO()
+    }
+}
